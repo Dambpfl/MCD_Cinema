521e8752e0044503b8026e1632ac4dcc2607468f
Damien <boenapfel.damien@yahoo.fr>
2024-12-10 15:38:46 +0100

    maj

---
+++ Projet/controller/CinemaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controller;
+use Model\Connect;
+
+class CinemaController {
+
+    /**
+     * Lister les films
+     */
+    public function listFilms() {
+        
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("SELECT titre, anneeSortie FROM film");
+        require "view/listFilms.php"
+    }
+}
