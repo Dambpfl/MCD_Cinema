d13ec013d9e7f09fca8b13045b1bf0b80f9c74cc
Damien <boenapfel.damien@yahoo.fr>
2025-01-05 21:49:49 +0100

    listFilms -> page de base

--- Projet/index.php
+++ Projet/index.php
@@ -8,35 +8,37 @@ spl_autoload_register(function ($class_name) {
 
 $ctrlCinema = new CinemaController();
 
-
+// verifie et recupere l'id via l'url
 $id = isset($_GET['id']) ? (int)$_GET['id'] : null;
 
+// listFilms = page de base
+$action = isset($_GET['action']) ? $_GET['action'] : 'listFilms';
 
-if(isset($_GET["action"])){
-    switch ($_GET["action"]) {
 
-        case "listFilms" : $ctrlCinema->listFilms(); break;
-        case "detailsFilm" : $ctrlCinema->detFilm($id); break;
+switch ($action) {
 
-        case "listActeurs" : $ctrlCinema->listActeurs(); break;
-        case "detailsActeur" : $ctrlCinema->detActeur($id); break;
+    case "listFilms" : $ctrlCinema->listFilms(); break;
+    case "detailsFilm" : $ctrlCinema->detFilm($id); break;
 
-        case "listRealisateurs" : $ctrlCinema->listRealisateurs(); break;
-        case "detailsRealisateur" : $ctrlCinema->detRealisateur($id); break;
+    case "listActeurs" : $ctrlCinema->listActeurs(); break;
+    case "detailsActeur" : $ctrlCinema->detActeur($id); break;
 
-        case "listGenres" : $ctrlCinema->listGenres(); break;
-        case "detailsGenre" : $ctrlCinema->detGenre($id); break;
+    case "listRealisateurs" : $ctrlCinema->listRealisateurs(); break;
+    case "detailsRealisateur" : $ctrlCinema->detRealisateur($id); break;
 
-        case "listRoles" : $ctrlCinema->listRoles(); break;
-        case "detailsRole" : $ctrlCinema->detRole($id); break;
+    case "listGenres" : $ctrlCinema->listGenres(); break;
+    case "detailsGenre" : $ctrlCinema->detGenre($id); break;
 
-        case "addGenre" : $ctrlCinema->addGenre(); break;
-        case "addActeur" : $ctrlCinema->addActeur(); break;
-        case "addFilm" : $ctrlCinema->addFilm(); break;
-        case "deleteActeur" : $ctrlCinema->deleteActeur(); break;
+    case "listRoles" : $ctrlCinema->listRoles(); break;
+    case "detailsRole" : $ctrlCinema->detRole($id); break;
 
-        case "search" : $ctrlCinema->search(); break;
-    }
+    case "addGenre" : $ctrlCinema->addGenre(); break;
+    case "addActeur" : $ctrlCinema->addActeur(); break;
+    case "addFilm" : $ctrlCinema->addFilm(); break;
+    case "deleteActeur" : $ctrlCinema->deleteActeur(); break;
+
+    case "search" : $ctrlCinema->search(); break;
 }
+
 
 ?>
